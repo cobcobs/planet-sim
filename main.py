0af512c0214659170a752b778e7ad0e6c85cd764
Jacob <joeyfrio.jf@gmail.com>
2022-02-25 19:13:51 +0800

    change floats to ints

--- main.py
+++ main.py
@@ -96,16 +96,16 @@ def main():
     sun.sun = True
 
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10 ** 24)
-    earth.y_velocity = 29.783 * 1000
+    earth.y_velocity = 29783
 
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10 ** 23)
-    mars.y_velocity = 24.077 * 1000
+    mars.y_velocity = 24077
 
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10 ** 23)
-    mercury.y_velocity = -47.4 * 1000
+    mercury.y_velocity = -47400
 
     venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10 ** 24)
-    venus.y_velocity = -35.02 * 1000
+    venus.y_velocity = -35020
 
     planets = [sun, earth, mars, mercury, venus]
 
